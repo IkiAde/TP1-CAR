a269268749fabd0eae23294c6de5b4f680ab8041
ikimath <ikimathadeoye@gmail.com>
2025-01-17 10:12:50 +0100

    code updated

--- TP1/src/connexion/Server.java
+++ TP1/src/connexion/Server.java
@@ -1,6 +1,6 @@
 package connexion;
 
-import java.io.Console;
+//import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,9 +12,9 @@ public class Server {
 	
 	
 	private static final String[][] USERS = {
-	        {"admin", "password123"},
-	        {"ikimath", "pass1"},
-	        {"guest", "guest123"}
+	        {"USER admin", "PASS password123"},
+	        {"USER ikimath", "PASS pass1"},
+	        {"USER guest", "PASS guest123"}
 	    };
 
 	 public static void main(String argv[]){
@@ -24,45 +24,47 @@ public class Server {
 		      while(true){
 		        Socket socket= server.accept();
 		        
-
+		        
 		        InputStream input=socket.getInputStream();	
 		        //afficher l'adresse du socket connecté
-		        System.out.println("New client "+ " "+ socket.getInetAddress()+"  connected");
+		        
 		        OutputStream output= socket.getOutputStream();
 		        
                 Scanner scanner = new Scanner(input);
+                //entrer le username
+                
+                output.write("220 Bienvenue\r\n".getBytes());
                 String username = scanner.nextLine();
-		        output.write("2121 Service ready\r\n".getBytes());
+		        System.out.println(username);
+		        output.write("331 User name okay, need password\r\n".getBytes());
 		        
-		     // Demander le mot de passe sans l'afficher à l'écran
-                Console console = System.console();
-                if (console == null) {
-                    System.out.println("Console is not available.");
-                    continue;
-                }
-		        
-		        
-		        output.write("password:\r\n".getBytes());
-		        char[] passwordArray = console.readPassword(); 
-                String password = new String(passwordArray);
+		     
+                String password = scanner.nextLine();
+                System.out.println(password);
                 
                 if (authenticate(username, password)) {
-                    output.write("220 Service ready\r\n".getBytes());
                     
+                	output.write("230 Auth réussie\r\n".getBytes());
                     // Utiliser le nom d'utilisateur dans le message
-                    output.write(("Name(localhost:" + username + ")\r\n").getBytes());
+                   
+                    String quit = scanner.nextLine();
+                    
+                    /* quitter la connexion */
+                    if (quit.equals("quit")) {
+                                output.write("221 Service closing control connection.\r\n".getBytes());
+                                socket.close();
+                    }
                   
                 } else {
-                    output.write("Authentication failed\r\n".getBytes());
+                	output.write("530 Authentication failed\r\n".getBytes());
                    
                 }
                 
-		        socket.close();
 		   	      
 
-		       }
+		       
 
-		    }
+		    }}
 
 		    catch (IOException e) {
 						e.getMessage();
